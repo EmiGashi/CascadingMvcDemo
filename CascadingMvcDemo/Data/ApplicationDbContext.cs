a4c8f7a86ff58fd111dea5854063884633b530a7
EmiGashi <eminelg@gmail.com>
2024-05-15 12:21:03 +0100

    Add project files.

---
+++ CascadingMvcDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CascadingMvcDemo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CascadingMvcDemo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Models.Category> Categories { get; set; }
+        public DbSet <Product> Products { get; set; }
+    }
+}
